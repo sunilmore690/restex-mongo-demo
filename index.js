6d738df19abfdb923ef001bc8c40a649fffa1350
Sunil More <sunilmore690@gmail.com>
2018-07-15 18:41:59 +0530

    handle error

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.send(err);
 });
 app.listen(3000, function() {
   console.log("restex-mongod-demo listening on 3000");
