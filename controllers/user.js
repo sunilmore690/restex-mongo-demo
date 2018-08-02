1f1ab89e0e1bcda4df1e62e6a29214802f79ea92
Sunil More <sunilmore690@gmail.com>
2018-08-02 14:38:01 +0530

    use of mongoose Model

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 let model_name = "users"; // make sure user schema exist in models dir
 module.exports = function(restex) {
   let UserDao = restex.dao(model_name);
+  let UserModel = restex.model(model_name)
   let authenticate = function(req, res, next) {
     //Using Promise then & catch
     UserDao.get({ email: req.body.email, password: req.body.password })
@@ -11,7 +12,17 @@ module.exports = function(restex) {
         return next(err);
       });
   };
+  let getCount = (req,res,next) => {
+    UserModel.count()
+    .then((count)=>{
+      res.json({count})
+    })
+    .catch((e)=>{
+      return next(e)
+    })
+  }
   return {
-    authenticate
+    authenticate,
+    getCount
   };
 };
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,4 @@
 module.exports = function(router) {
   router.post("/authenticate", "user#authenticate");
+  router.get("/usercount", "user#getCount");
 };
