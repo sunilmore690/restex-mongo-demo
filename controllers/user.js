61df748e5693956b896ef84b2d2715a79169c294
Sunil More <sunilmore690@gmail.com>
2018-08-02 14:17:43 +0530

    method name changes

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 let model_name = "users"; // make sure user schema exist in models dir
 module.exports = function(restex) {
-  let UserDao = restex.model(model_name);
+  let UserDao = restex.dao(model_name);
   let authenticate = function(req, res, next) {
     //Using Promise then & catch
     UserDao.get({ email: req.body.email, password: req.body.password })
